180b48b251deef3110c1ebb143d74add7147d88e
nonso4l <casomugha80@gmail.com>
2024-12-17 21:30:50 +0000

    When executed, your script should print the simple interest calculated with the provided values.

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,7 +3,4 @@ principal = 1000
 rate = 0.05
 time = 3
 interest = (principal*rate*time)
-print("principal", principal)
-print("rate", rate)
-print("time", time)
-print("The simple interest is:" ,interest)
+print(f"The simple interest is: {interest}")
