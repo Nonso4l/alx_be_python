bf3528ba59c701f87c223c4e588485d32151ea8f
nonso4l <casomugha80@gmail.com>
2024-12-30 17:56:05 +0000

    a script named daily_reminder.py. This script will ask the user for a single task, its priority level, and if it is time-sensitive. The program will then provide a customized reminder for that task, demonstrating control flow and loops without relying on data structures to store multiple tasks.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
-task_variable = (input("Enter a task description: "))
-task_priority = (input("Enter priority (high/medium/low): " )).lower()
-time_bound = (input("Is it time bound ? (yes/no): ")).lower()
+task_variable = input("Enter a task description: ")
+task_priority = input("Enter priority (high/medium/low): " ).lower()
+time_bound = input("Is it time bound ? (yes/no): ").lower()
 
 match task_priority:
     case "high":
@@ -13,7 +13,7 @@ match task_priority:
         level = "low"
 
     case _:
-        print("Invalid priority task")
+        level = "Invalid priority task"
 
 if time_bound == "yes":
     print(f"Reminder: {task_variable} is a {level} priority task that requires immediate attention today! ")
